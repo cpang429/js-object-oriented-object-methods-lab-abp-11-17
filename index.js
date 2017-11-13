71bcc36bd50a41496b989c45aabd3347e945af54
Chris Pang <chris_pang429@outlook.com>
2017-11-13 23:57:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 function BoardMember(name, homeState, training) {
-    this.name = name;
+    /*this.name = name;
     this.homeState = homeState;
     this.training = training;
     this.veto = function(){
@@ -17,5 +17,5 @@ function BoardMember(name, homeState, training) {
     }
     this.sayHi = function(){
       return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}`;
-    } 
+    } */
 }
