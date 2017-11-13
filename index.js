d483c3501c39149d117acda978a100d478587095
Chris Pang <chris_pang429@outlook.com>
2017-11-13 23:58:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 function BoardMember(name, homeState, training) {
-    /*this.name = name;
+    this.name = name;
     this.homeState = homeState;
     this.training = training;
-    this.veto = function(){
+  /*  this.veto = function(){
       return "No, I must disagree";
     }
     this.approve = function(){
