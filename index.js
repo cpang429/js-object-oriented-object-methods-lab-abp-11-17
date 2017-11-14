d926fbe172d604015d4cfaeecdc985b4ab77cc96
Chris Pang <chris_pang429@outlook.com>
2017-11-14 00:04:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ function BoardMember(name, homeState, training) {
     this.name = name;
     this.homeState = homeState;
     this.training = training;
-  /* this.veto = function(){
+    this.veto = function(){
       return "No, I must disagree";
-    }
+    } 
     this.approve = function(){
       return "You can do that!";
     }
     this.doCharity = function(){
       return "I like to help people.";
-    }
+    } /*
     this.releasePressStatement(){
       return "You will see great things from Scuber.";
     }
